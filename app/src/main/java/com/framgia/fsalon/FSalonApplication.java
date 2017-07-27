03c96577fc07da2fed38d14d9d2d60ec61442703
ongnau <beepig025@gmail.com>
2017-07-27 11:39:10 +0700

    test ci

--- app/src/main/java/com/framgia/fsalon/FSalonApplication.java
+++ app/src/main/java/com/framgia/fsalon/FSalonApplication.java
@@ -4,17 +4,31 @@ import android.app.Application;
 
 import com.framgia.fsalon.data.source.api.FSalonServiceClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by MyPC on 20/07/2017.
  */
 public class FSalonApplication extends Application {
     private static FSalonApplication sInstant;
+    private final static int a = 10;
 
     @Override
     public void onCreate() {
         super.onCreate();
         FSalonServiceClient.initialize(this);
         sInstant = this;
+        List<String> a = new ArrayList<>();
+        a.add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        a.add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        a.add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        a.add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        a.add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        a.add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        a.add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aisjdiasd sadijaisjd iasjd " +
+            "iasjdiasjdiasjdiasjdiasdjisadjiasdjiasdjasidjasidjsai " +
+            "asdsadasdasdasdasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssdjasid");
     }
 
     public static FSalonApplication getInstant() {
